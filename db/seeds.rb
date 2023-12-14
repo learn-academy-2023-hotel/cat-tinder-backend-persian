783236d60aeb662109d8feab97f63e072d854d79
LEARN Academy <learnacademy@MacBook-Air.local>
2023-12-13 16:14:03 -0800

    updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,21 +3,21 @@ cats = [
                  name: 'Mako',
                  age: 2,
                  enjoys: 'annoying you when youre working',
-                 image: 'https://unsplash.com/photos/russian-blue-cat-wearing-yellow-sunglasses-yMSecCHsIBc'
+                 image: 'https://images.unsplash.com/photo-1533738363-b7f9aef128ce?q=80&w=2835&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         
          },
          {
                  name: 'Remii',
                  age: 1,
                  enjoys: 'running around at night',
-                 image: 'https://unsplash.com/photos/white-and-brown-long-fur-cat-ZCHj_2lJP00'
+                 image: 'https://images.unsplash.com/photo-1592194996308-7b43878e84a6?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
             
         },
         {
                  name: 'Garfield',
                  age: 32,
                  enjoys: 'lasagna',
-                 image: 'https://unsplash.com/photos/shallow-focus-photography-of-orange-persian-cat-hjsNyctCkCQ'
+                 image: 'https://images.unsplash.com/photo-1512616643169-0520ad604fc2?q=80&w=2835&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
             
         }
 ]
